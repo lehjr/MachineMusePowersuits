9c7aee916025e6a8d78d0ea67d88dbea36755e55
Leon <lehjr1@gmail.com>
2021-08-18 18:17:39 -0400

    added a recipe group for sorting tiered module recipes

---
+++ src/main/java/com/github/lehjr/numina/util/client/recipe/ModuleRecipeGroup.java
@@ -0,0 +1,24 @@
+package com.github.lehjr.numina.util.client.recipe;
+
+import net.minecraft.client.gui.recipebook.RecipeList;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+
+import javax.annotation.Nonnull;
+
+/**
+ * Just a quick and dirty way of adding the tier and "group" to the recipe list for faster sorting
+ * None of the parent fields need to be accessible since they're still populated the same way
+ */
+@OnlyIn(Dist.CLIENT)
+public class ModuleRecipeGroup extends RecipeList {
+    final int tier;
+    @Nonnull
+    final String group;
+
+    public ModuleRecipeGroup(RecipeList iRecipes, int tier, @Nonnull String group) {
+        super(iRecipes.getRecipes());
+        this.tier = tier;
+        this.group = group;
+    }
+}
