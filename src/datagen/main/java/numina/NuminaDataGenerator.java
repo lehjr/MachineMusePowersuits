1380e95d6510c35dbcf7602b14f10eb3aa01027f
Leon <lehjr1@gmail.com>
2022-03-27 21:59:07 -0400

    add code for helping clean up translations

---
+++ src/datagen/main/java/numina/NuminaDataGenerator.java
@@ -0,0 +1,30 @@
+package numina;
+
+import numina.client.lang.NuminaLangProvider;
+import lehjr.numina.constants.NuminaConstants;
+import net.minecraft.data.DataGenerator;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.GatherDataEvent;
+
+@Mod.EventBusSubscriber(modid = NuminaConstants.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class NuminaDataGenerator {
+    private NuminaDataGenerator() {
+    }
+
+    @SubscribeEvent
+    public static void gatherData(GatherDataEvent event) {
+        DataGenerator gen = event.getGenerator();
+        ExistingFileHelper existingFileHelper = event.getExistingFileHelper();
+        if (event.includeClient()) {
+            //Client side data generators
+            gen.addProvider(new NuminaLangProvider(gen, existingFileHelper, NuminaConstants.MOD_ID, "main"));
+        }
+        if (event.includeServer()) {
+            //Server side data generators
+
+        }
+    }
+}
+
