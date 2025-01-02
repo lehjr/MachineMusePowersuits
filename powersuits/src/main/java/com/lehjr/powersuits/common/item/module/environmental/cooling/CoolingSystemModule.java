8d10d5207a130078d1769b6f422284b1db2821fd
lehjr <lehjr1@gmail.com>
2025-01-01 20:39:05 -0500

    work on merging cooling system model with tank

--- powersuits/src/main/java/com/lehjr/powersuits/common/item/module/environmental/cooling/CoolingSystemModule.java
+++ powersuits/src/main/java/com/lehjr/powersuits/common/item/module/environmental/cooling/CoolingSystemModule.java
@@ -4,6 +4,7 @@ import com.lehjr.numina.common.capabilities.module.powermodule.ModuleCategory;
 import com.lehjr.numina.common.capabilities.module.powermodule.ModuleTarget;
 import com.lehjr.numina.common.capabilities.module.tickable.PlayerTickModule;
 import com.lehjr.numina.common.registration.NuminaCodecs;
+import com.lehjr.numina.common.utils.ElectricItemUtils;
 import com.lehjr.numina.common.utils.HeatUtils;
 import com.lehjr.powersuits.common.constants.MPSConstants;
 import com.lehjr.powersuits.common.item.module.AbstractPowerModule;
@@ -32,24 +33,27 @@ import javax.annotation.Nonnull;
  * Created by Eximius88 on 1/17/14.
  */
 public class CoolingSystemModule extends AbstractPowerModule {
-
-
-
-
-    // FIXME: how to mix fluid handler with Player Tick module while still passing both capability checks
-
-
-
-
-
-
     public static class FluidHandlerItemStackMPS extends FluidHandlerItemStack {
         /**
-         * @param container     The container itemStack, data is stored on it directly under a component.
-         * @param capacity      The maximum capacity of this fluid tank. Set with config value based on tier
+         * @param container The container itemStack, data is stored on it directly under a component.
+         * @param tier
          */
-        public FluidHandlerItemStackMPS(ItemStack container, int capacity) {
-            super(()-> NuminaCodecs.ITEM_FLUID_CODEC, container, capacity);
+        public FluidHandlerItemStackMPS(ItemStack container, int tier) {
+            super(() -> NuminaCodecs.ITEM_FLUID_CODEC, container, getCapacity(tier));
+        }
+
+        /*
+            returns The maximum capacity of this fluid tank. Set with config value based on tier
+         */
+        static int getCapacity(int tier) {
+            return switch (tier) {
+                // FIXME: set up proper config values
+                case 1 -> 1;
+                case 2 -> 2;
+                case 3 -> 3;
+                case 4 -> 4;
+                default -> 0;
+            };
         }
 
         // TODO: different fluids for different tier modules
@@ -58,55 +62,75 @@ public class CoolingSystemModule extends AbstractPowerModule {
             return fluid.getFluid() == Fluids.WATER;
         }
     }
-//
-//
-//
-//    // TODO: merge tank code with cooling system code, but with separate fluid cap
-//    public static class Ticker extends PlayerTickModule {
-//        int tier;
-//        public Ticker(@Nonnull ItemStack module, int tier) {
-//            super(module, ModuleCategory.ENVIRONMENTAL, ModuleTarget.TORSOONLY);
-//            this.tier = tier;
 
-    //        }
-//
-//        @Override
-//        public void onPlayerTickActive(Player player, @Nonnull ItemStack item) {
-//            double heatBefore = HeatUtils.getPlayerHeat(player);
-//
-//            HeatUtils.coolPlayer(player, /*0.1 * */ applyPropertyModifiers(MPSConstants.COOLING_BONUS));
-//            double cooling = heatBefore - HeatUtils.getPlayerHeat(player);
-//            ElectricItemUtils.drainPlayerEnergy(player, (int) (cooling * applyPropertyModifiers(MPSConstants.ENERGY_CONSUMPTION)), false);
-//        }
-//
-//        @Override
-//        public int getTier() {
-//            return tier;
-//        }
-//    }
-//
-    static final String FLUID_NBT_KEY = "Fluid";
+    /*
+     * Cooling system tier/types
+     * -------------------------
+     * 1) passive: bucket of water (no power consumption, just a trigger value)
+     * 2) passive: tank of water (increased capacity, no power consumption, just a trigger value)
+     * 3) active: tank and circulation pump (increased capacity, powered circulation pump, with a trigger value)
+     * 4) becomes part of top tier thermal generator?
+     */
+
     // one heat unit per 5 mB of water
-    static final double coolingFactor = 1.0/5.0;
+    static final double coolingFactor = 1.0 / 5.0;
 
-    public static class Ticker2 extends PlayerTickModule {
-        public Ticker2(@Nonnull ItemStack module) {
+    public static class Ticker extends PlayerTickModule {
+        int tier;
+
+        public Ticker(@Nonnull ItemStack module, int tier) {
             super(module, ModuleCategory.ENVIRONMENTAL, ModuleTarget.TORSOONLY);
-//            addBaseProperty(MPSConstants.FLUID_TANK_SIZE, 20000);
-            addBaseProperty(MPSConstants.HEAT_ACTIVATION_PERCENT, 0.5);
-            addTradeoffProperty(MPSConstants.ACTIVATION_PERCENT, MPSConstants.HEAT_ACTIVATION_PERCENT, 0.5, "%");
-            addTradeoffProperty(MPSConstants.ENERGY_CONSUMPTION, MPSConstants.COOLING_BONUS, 1, "%");
-            addTradeoffProperty(MPSConstants.ENERGY_CONSUMPTION, MPSConstants.ENERGY_CONSUMPTION, 40, "RF/t");
+            this.tier = tier;
+            switch (tier) {
+                case 1: {
+                    // FIXME: set up proper config values
+                    addBaseProperty(MPSConstants.HEAT_ACTIVATION_PERCENT, 0.5);
+                    addTradeoffProperty(MPSConstants.ACTIVATION_PERCENT, MPSConstants.HEAT_ACTIVATION_PERCENT, 0.5, "%");
+                    addTradeoffProperty(MPSConstants.ENERGY_CONSUMPTION, MPSConstants.COOLING_BONUS, 1, "%");
+                    addTradeoffProperty(MPSConstants.ENERGY_CONSUMPTION, MPSConstants.ENERGY_CONSUMPTION, 40, "RF/t");
+                }
+                case 2: {
+                    addBaseProperty(MPSConstants.HEAT_ACTIVATION_PERCENT, 0.5);
+                    addTradeoffProperty(MPSConstants.ACTIVATION_PERCENT, MPSConstants.HEAT_ACTIVATION_PERCENT, 0.5, "%");
+                    addTradeoffProperty(MPSConstants.ENERGY_CONSUMPTION, MPSConstants.COOLING_BONUS, 1, "%");
+                    addTradeoffProperty(MPSConstants.ENERGY_CONSUMPTION, MPSConstants.ENERGY_CONSUMPTION, 40, "RF/t");
+                }
+                case 3: {
+                    addBaseProperty(MPSConstants.HEAT_ACTIVATION_PERCENT, 0.5);
+                    addTradeoffProperty(MPSConstants.ACTIVATION_PERCENT, MPSConstants.HEAT_ACTIVATION_PERCENT, 0.5, "%");
+                    addTradeoffProperty(MPSConstants.ENERGY_CONSUMPTION, MPSConstants.COOLING_BONUS, 1, "%");
+                    addTradeoffProperty(MPSConstants.ENERGY_CONSUMPTION, MPSConstants.ENERGY_CONSUMPTION, 40, "RF/t");
+                }
+                case 4: {
+                    addBaseProperty(MPSConstants.HEAT_ACTIVATION_PERCENT, 0.5);
+                    addTradeoffProperty(MPSConstants.ACTIVATION_PERCENT, MPSConstants.HEAT_ACTIVATION_PERCENT, 0.5, "%");
+                    addTradeoffProperty(MPSConstants.ENERGY_CONSUMPTION, MPSConstants.COOLING_BONUS, 1, "%");
+                    addTradeoffProperty(MPSConstants.ENERGY_CONSUMPTION, MPSConstants.ENERGY_CONSUMPTION, 40, "RF/t");
+                }
+            }
+        }
+
+        @Override
+        public boolean isAllowed() {
+            // FIXME: set up proper config values
+            return switch (this.tier) {
+                case 1 -> true;
+                case 2 -> true;
+                case 3 -> true;
+                case 4 -> true;
+                default -> false;
+            };
         }
 
         @Override
         public void onPlayerTickActive(Player player, Level level, @Nonnull ItemStack item) {
             IFluidHandlerItem fluidHandler = getFluidHandler(getModule());
-            if (fluidHandler == null ) {
+            if (fluidHandler == null) {
                 return;
             }
 
-            if (/*player.world.isRemote() &&*/ player.getCommandSenderWorld().getGameTime() % 10 == 0 ) {
+            // Fill if player in some sort of water
+            if (/*player.world.isRemote() &&*/ player.getCommandSenderWorld().getGameTime() % 10 == 0) {
                 // we only have one tank, so index 0;
                 int maxFluid = fluidHandler.getTankCapacity(0);
                 int currentFluid = fluidHandler.getFluidInTank(0).getAmount();
@@ -120,7 +144,7 @@ public class CoolingSystemModule extends AbstractPowerModule {
                     if (player.isInWater() && level.getBlockState(pos).getBlock() != Blocks.BUBBLE_COLUMN) {
                         if (blockstate.getBlock() instanceof BucketPickup && blockstate.getFluidState().getType() == Fluids.WATER) {
                             FluidActionResult pickup = FluidUtil.tryPickUpFluid(getModule(), player, level, pos, null); // fixme?
-                            if(pickup.isSuccess()) {
+                            if (pickup.isSuccess()) {
                                 FluidStack water = new FluidStack(Fluids.WATER, 1000);
                                 if (fluidHandler.fill(water, IFluidHandler.FluidAction.EXECUTE) > 0) {
                                     player.playSound(SoundEvents.BUCKET_FILL, 1.0F, 1.0F);
@@ -142,26 +166,41 @@ public class CoolingSystemModule extends AbstractPowerModule {
             double maxHeat = HeatUtils.getPlayerMaxHeat(player);
             if ((currentHeat / maxHeat) >= applyPropertyModifiers(MPSConstants.HEAT_ACTIVATION_PERCENT)) {
 
-                // cool 200 per bucket
-                double coolAmount = fluidHandler.drain(
-                                // adjust so cooling does not exceed cooling needed
-                                (int) Math.min(1000, currentHeat/coolingFactor),
-                                // only execute on server, simulate on client
-                                level.isClientSide ? IFluidHandler.FluidAction.SIMULATE : IFluidHandler.FluidAction.EXECUTE)
-                        .getAmount() * coolingFactor;
+                // cool from electric system first
+                double coolAmount = Math.min(applyPropertyModifiers(MPSConstants.COOLING_BONUS), currentHeat);
+                double energyUsage = coolAmount * applyPropertyModifiers(MPSConstants.ENERGY_CONSUMPTION);
+                if (ElectricItemUtils.getPlayerEnergy(player) >= energyUsage) {
+                    HeatUtils.coolPlayer(player, /*0.1 * */ applyPropertyModifiers(MPSConstants.COOLING_BONUS));
+                    currentHeat = HeatUtils.getPlayerHeat(player);
+                    ElectricItemUtils.drainPlayerEnergy(player, (int) (energyUsage), false);
 
-                HeatUtils.coolPlayer(player, coolAmount);
-                if (coolAmount > 0) {
-                    level.playSound(player, player.blockPosition(), SoundEvents.GENERIC_EXTINGUISH_FIRE, SoundSource.MASTER, 1.0F, 1.0F);
-                    for (int i = 0; i < 4; i++) {
-                        level.addAlwaysVisibleParticle(ParticleTypes.SMOKE, player.getX(), player.getY() + 0.5, player.getZ(), 0.0D, 0.0D, 0.0D);
+                    if (coolAmount > 0) {
+                        level.playSound(player, player.blockPosition(), SoundEvents.GENERIC_EXTINGUISH_FIRE, SoundSource.MASTER, 1.0F, 1.0F);
+                        for (int i = 0; i < 4; i++) {
+                            level.addAlwaysVisibleParticle(ParticleTypes.SMOKE, player.getX(), player.getY() + 0.5, player.getZ(), 0.0D, 0.0D, 0.0D);
+                        }
+                    }
+
+                    // cool from liquid at 200 per bucket
+                    coolAmount = fluidHandler.drain(
+                        // adjust so cooling does not exceed cooling needed
+                        (int) Math.min(1000, currentHeat / coolingFactor),
+                        // only execute on server, simulate on client
+                        level.isClientSide ? IFluidHandler.FluidAction.SIMULATE : IFluidHandler.FluidAction.EXECUTE).getAmount() * coolingFactor;
+                    HeatUtils.coolPlayer(player, coolAmount);
+                    if (coolAmount > 0) {
+                        level.playSound(player, player.blockPosition(), SoundEvents.GENERIC_EXTINGUISH_FIRE, SoundSource.MASTER, 1.0F, 1.0F);
+                        for (int i = 0; i < 4; i++) {
+                            level.addAlwaysVisibleParticle(ParticleTypes.SMOKE, player.getX(), player.getY() + 0.5, player.getZ(), 0.0D, 0.0D, 0.0D);
+                        }
                     }
                 }
             }
-        }
-    }
 
-    static IFluidHandlerItem getFluidHandler(@Nonnull ItemStack module) {
-        return module.getCapability(Capabilities.FluidHandler.ITEM);
+        }
+
+        static IFluidHandlerItem getFluidHandler(@Nonnull ItemStack module) {
+            return module.getCapability(Capabilities.FluidHandler.ITEM);
+        }
     }
 }
